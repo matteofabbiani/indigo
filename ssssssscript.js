3283935077b7395fbfee86ab2e8d23d6bebe714c
matteofabbiani <50113523+matteofabbiani@users.noreply.github.com>
2024-03-14 21:49:24 +0100

    Update and rename sssssscript.js to ssssssscript.js

--- ssssssscript.js
+++ ssssssscript.js
@@ -60,3 +60,77 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Swiper slider
+$(".slider-main_component").each(function(index) {
+  let loopMode = false;
+  if ($(this).attr("loop-mode") === "true") {
+    loopMode = true;
+  }
+  let sliderDuration = 300;
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
+  }
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoHeight: false,
+    centeredSlides: loopMode,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 1,
+    spaceBetween: "0%",
+    rewind: false,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true
+    },
+    breakpoints: {
+      // mobile portrait
+      1: {
+        slidesPerView: 1,
+        spaceBetween: "0%"
+      },
+      // mobile landscape
+      480: {
+        slidesPerView: 1,
+        spaceBetween: "0%"
+      },
+      // tablet
+      768: {
+        slidesPerView: 1,
+        spaceBetween: "0%"
+      },
+      // desktop
+      992: {
+        slidesPerView: 1,
+        spaceBetween: "0%"
+      }
+    },
+    pagination: {
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "button",
+      clickable: true
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+    scrollbar: {
+      el: $(this).find(".swiper-drag-wrapper")[0],
+      draggable: true,
+      dragClass: "swiper-drag",
+      snapOnRelease: true
+    },
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
+  });
+});
+
